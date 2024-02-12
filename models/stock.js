0dadf1ef5d7ba7d28a767e031db8e8fe07a35d78
Riyan <riyan04314@gmail.com>
2024-02-12 23:38:22 +0530

    mongoose

--- models/stock.js
+++ models/stock.js
@@ -2,6 +2,10 @@ const mongoose  = require('mongoose');
 const Schema    = mongoose.Schema;
 
 const stockSchema = new Schema({
+  sid:{
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
